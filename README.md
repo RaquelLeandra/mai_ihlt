2038a22d64bd0d0e49b8dd543eb76f67fe468860
Jorge Sierra <jsierra.siete@gmail.com>
2018-09-16 17:49:57 +0200

    Added .gitignore, readme and generated .py files from .ipynb (Jupyter Notebook)

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# mai_iklt
+# Projects for Introduction to Human Language Technology
+
+* Raquel Pérez
+* Jorge Sierra Acosta
+
+*Excersices are done using Python, NLTK, and the Jupyter Notebook*
+
+[Link](https://www.fib.upc.edu/en/studies/masters/master-artificial-intelligence/curriculum/syllabus/IHLT-MAI)
+
+The goal of this course is to provide the fundamentals of Natural Language Processing (NLP) to the student. Concretely, the course is an introduction to the most relevant drawbacks involved in NLP, the most relevant techniques and resources used to tackle with them, and the theories they are based on. In addition, brief descriptions of the most relevant NLP applications are included. The course will focus on knowledge-based and empirical-based approaches to NLP (both statistical and machine learning).
+
+### Projects:
+
+| Session    | Excersice    | Description |
+|------------|--------------|-------------|
+| Session 1  |              | Introduction to lab |
+| Session 2  | Mandatory    | Measure correlation between sentences |
+| Session 2  | Optional     | Language classifier |
---
+++ session2/mandatory.py
@@ -0,0 +1,72 @@
+
+# coding: utf-8
+
+# # Mandatory excersice
+
+# In[1]:
+
+
+import nltk
+import urllib.request
+from nltk.metrics import jaccard_distance
+from scipy.stats import pearsonr
+
+
+# **Read all pairs of sentences of the trial set within the
+# evaluation framework of the project.**
+
+# In[2]:
+
+
+pairs = {}
+
+with open('trial/STS.input.txt', 'r') as f:
+    for l in f:
+        sid = l.split('\t')[0]
+        s1  = l.split('\t')[1]
+        s2  = l.split('\t')[2][:-1] # Remove the \n character
+        pairs[sid] = [s1, s2]
+        print(sid, pairs[sid])
+
+
+# **Compute their similarities by considering words and Jaccard distance.**
+# 
+
+# In[3]:
+
+
+simmilarities = []
+for sid in pairs:
+    s1      = pairs[sid][0]
+    s2      = pairs[sid][1]
+    words_1 = set([word.lower() for word in nltk.word_tokenize(s1)])
+    words_2 = set([word.lower() for word in nltk.word_tokenize(s2)])
+    jaccard_simmilarity = 1 - jaccard_distance(words_1, words_2)
+    simmilarities.append(jaccard_simmilarity) 
+    print('id:', sid, 'distance:', jaccard_simmilarity)
+
+
+# **Compare the previous results with gold standard by giving the pearson correlation between them.**
+
+# In[4]:
+
+
+gs = {}
+
+with open('trial/STS.gs.txt', 'r') as f:
+    for l in f:
+        sid     = l.split('\t')[0]
+        value   = abs( int(l.split('\t')[1])-5)    
+        gs[sid] = value
+
+refs = list(gs.values())
+print(pearsonr(refs, simmilarities)[0])
+
+
+# We obtain a correlation coefficient of 0.5140573923420935. 
+# 
+# As we are comparing two arrays of similarity values, we obtain a positive correlation. 
+# 
+# This value is a little bigger than 0.5, this means that there is little correlation between the two arrays, so probably the Jaccard distance isn't the best way to measure the semantic similarity between sentences. 
+# 
+# These results are due to the definition of Jaccard distance. This definition is fully based on set theory and does not take into account the semantic relationship between words (like synonymity).
